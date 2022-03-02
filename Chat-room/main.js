2fcb2247887c282253105326de43f344ab6417c6
Lacedaimon95 <svetozar95@gmail.com>
2022-03-02 14:54:49 +0100

    #27 commit

---
+++ Chat-room/main.js
@@ -0,0 +1,32 @@
+let dropBtn = document.querySelector("#dropBtn");
+let buttons = document.querySelectorAll("#buttons button")
+// let resolution = window.matchMedia("(max-width: 768px)");
+
+
+// resolution.addEventListener("change",e =>{
+
+//     if(resolution.matches){
+//         icon.addEventListener("click",e =>{
+//             e.preventDefault();
+        
+//             if(icon.classList.contains("of")){
+//                 buttons.forEach(e=>{
+//                     e.style.display = "block";
+//                 })
+//                 icon.classList.remove("of");
+        
+//             }else if(!icon.classList.contains("of")){
+//             buttons.forEach(e=>{
+//                 e.style.display = "none";
+//             })
+//             icon.classList.add("of");
+//            }
+        
+//         })
+
+//     }else{
+//         buttons.forEach(e=>{
+//             e.style.display = "block";
+//         })
+//     }
+// })
