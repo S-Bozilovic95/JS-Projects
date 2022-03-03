718eead59fb6ce7394d3e6f196884dc8dbf70582
Lacedaimon95 <svetozar95@gmail.com>
2022-03-03 01:00:54 +0100

    #31 commit

--- Chat-room/main.js
+++ Chat-room/main.js
@@ -1,33 +1,7 @@
-let dropBtn = document.querySelector("#dropBtn");
-let buttons = document.querySelectorAll("#buttons button")
-// let resolution = window.matchMedia("(max-width: 768px)");
+import chatroom from "./chat.js"
+
+let chatroom1 = new chatroom("js","Svetozar");
+
+console.log(chatroom1);
 
 
-// resolution.addEventListener("change",e =>{
-
-//     if(resolution.matches){
-//         icon.addEventListener("click",e =>{
-//             e.preventDefault();
-        
-//             if(icon.classList.contains("of")){
-//                 buttons.forEach(e=>{
-//                     e.style.display = "block";
-//                 })
-//                 icon.classList.remove("of");
-        
-//             }else if(!icon.classList.contains("of")){
-//             buttons.forEach(e=>{
-//                 e.style.display = "none";
-//             })
-//             icon.classList.add("of");
-//            }
-//         })
-
-//     }else{
-//         buttons.forEach(e=>{
-//             e.style.display = "block";
-//         })
-//     }
-// })
-
-console.log(db);
